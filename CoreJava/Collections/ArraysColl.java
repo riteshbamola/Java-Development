89dda3e235197871487b2dd813a42681fda79019
Ritesh Bamola <rishubamola01@gmail.com>
2026-03-13 17:05:34 +0530

    Collections Practice

--- CoreJava/Collections/ArraysColl.java
+++ CoreJava/Collections/ArraysColl.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public class Arrays{
+public class ArraysColl {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(12);
@@ -15,5 +16,28 @@ public class Arrays{
 
         list.addAll(temp);
         System.out.println(list);
+
+        list.set(0,100);   //set 100 at 0
+        System.out.println(list.contains(54));
+
+
+        /*
+        list.remove(1);  //remove index 1    O(n)
+        list.remove(Integer.valueOf(14));    //remove value 14
+        System.out.println(list);
+
+        list.clear();   //clear the list
+        */
+
+        for(Integer i:list){
+            System.out.println(i);
+        }
+
+        Iterator<Integer> it = list.iterator();
+
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
     }
 }
--- CoreJava/Collections/Tasks.java
+++ CoreJava/Collections/Tasks.java
@@ -1,4 +1,51 @@
 package CoreJava.Collections;
 
+import java.util.*;
+
 public class Tasks {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(121);
+        list.add(34);
+        list.add(67);
+        list.add(3);
+        list.add(17);
+        list.add(121);
+        list.add(3);
+
+        System.out.println(list);
+//        Integer sum=0;
+
+        //max without Collections
+//        for(Integer it: list){
+//            sum= Math.max(sum,it);
+//        }
+
+        //with collections
+//        System.out.println(Collections.max(list));
+
+//        Collections.reverse(list);   reverse list
+//        Collections.sort(list);     sorting
+
+        Collections.sort(list);
+        int idx=0;
+        for(int i=1;i<list.size();i++){
+            if(list.get(i) != list.get(idx)){
+                list.set(idx+1,list.get(i));
+                idx++;
+            }
+        }
+
+
+        Set<Integer> set = new HashSet<>();
+        set.add(new Integer(34));
+        set.add(new Integer(34));
+        System.out.println(set);
+        Thread th = new Thread();
+
+
+
+        System.out.println(list);
+    }
 }
